cfa7cbb1294714db1a5048c8643c51497031f629
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-20 23:11:52 -0700

    added multiplication formula

---
+++ sum.js
@@ -0,0 +1,10 @@
+"use strict";
+var num1 = 10;
+var num2 = 20;
+var sum = num1 + num2;
+console.log(sum);
+// multiplication
+var num1 = 20;
+var num2 = 30;
+var result = num1 * num2;
+console.log(result);
---
+++ sum.ts
@@ -0,0 +1,10 @@
+var num1:number = 10
+var num2:number = 20
+var sum:number = num1+num2
+console.log(sum)
+
+// multiplication
+var num1:number=20
+var num2:number=30
+var result:number=num1*num2
+console.log(result)
